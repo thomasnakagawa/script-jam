359e707b2b1326a07c89b35889927c05ff45ae41
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-04-03 01:14:16 -0700

    Refactor and add more sounds

--- src/App.js
+++ src/App.js
@@ -3,18 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 import Sampler from './components/Sampler';
 import Sequencer from './components/Sequencer';
+import SoundPlayer from './components/SoundPlayer';
+import PlayerControls from './components/PlayerControls';
+
+const playableKeys = ["z", "x", "c", "v", "a", "s", "d", "f", "q", "w", "e", "r", "1", "2", "3", "4"]
+const playableKeys2 = ["b", "n", "m", ",", "g", "h", "j", "k", "t", "y", "u", "i", "5", "6", "7", "8"]
 
 function App() {
   const [tempo, setTempo] = useState(120);
+  const [currentTrack, setCurrentTrack] = useState(0);
   const [virtualKeys, setVirtualKeys] = useState([]);
+  const [userKey, setUserKey] = useState(null);
+  const [markerConfig, setMarker] = useState({ line: 0, char: 0, show: false });
+
   const [tracks, updateTracks] = useState([{
     type: "pitch",
-    code: "adsfa\nadfa"
+    code:
+`q
+ e
+d
+ s
+ s
+d f 
+d  d
+ 1`
   }]);
   return (
     <div className="App">
-      <Sampler tempo={tempo} virtualKeys={ virtualKeys }/>
-      <Sequencer tempo={tempo} onVirtualKeys={ setVirtualKeys }/>
+      <Sampler
+        playableKeys={ playableKeys }
+        virtualKeys={ virtualKeys }
+        onKeyDown={ key => {
+          setUserKey(key);
+        } }
+        onKeyUp={ key => {
+          setUserKey(null);
+        } }
+      />
+
+      <PlayerControls
+        tempo={ tempo }
+        onVirtualKeys={ setVirtualKeys }
+        tracks={ tracks }
+        onMarkerChange={ setMarker }
+      />
+
+      <Sequencer
+        code={ tracks[currentTrack].code }
+        onCodeChange={ newCode => {
+          const updatedTracks = tracks;
+          updatedTracks[currentTrack].code = newCode;
+          updateTracks(updatedTracks);
+        } }
+        markerConfig={ markerConfig }
+      />
+
+      <SoundPlayer
+        playableKeys={ playableKeys }
+        playableKeys2={ playableKeys2 }
+        userKey={ userKey }
+        virtualKeys={ virtualKeys }
+      />
     </div>
   );
 }
--- src/components/Sequencer.js
+++ src/components/Sequencer.js
@@ -8,102 +8,32 @@ export default class Sequencer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      isPlaying: false,
-      currentPlayingLine: -1,
-      code: `v\nx\nz\nc\n\ns`,
-      playbackInterval: null,
-      runInterval: null
-    }
-
-    this.handleBeat = this.handleBeat.bind(this);
-    this.handlePlay = this.handlePlay.bind(this);
-    this.handleStop = this.handleStop.bind(this);
     this.getMarkers = this.getMarkers.bind(this);
   }
 
-  componentWillUnmount() {
-    if (this.state.playbackInterval) {
-      clearInterval(this.state.playbackInterval);
-    }
-    if (this.state.runInterval) {
-      clearInterval(this.state.runInterval);
-    }
-  }
-
-  handleBeat() {
-    if (this.runInterval) {
-      clearInterval(this.runInterval);
-      this.setState({ runInterval: null });
-    }
-
-    const lineSplit = this.state.code.split(/\r\n|\r|\n/);
-    const nextLineIndex = (this.state.currentPlayingLine + 1) % lineSplit.length;
-    this.setState({ currentPlayingLine: nextLineIndex });
-
-    const currentLine = lineSplit[nextLineIndex];
-
-    if (currentLine.length > 0) {
-      this.props.onVirtualKeys([currentLine.charAt(0)]);
-      if (currentLine.length > 1) {
-        // play a run of multiple notes
-        const intervalLength = (60000 / this.props.tempo) / currentLine.length;
-        let charIndex = 0;
-        const newRunInterval = setInterval(() => {
-          charIndex += 1;
-          if (charIndex < currentLine.length) {
-            this.props.onVirtualKeys([currentLine.charAt(charIndex)]);
-          }
-        }, intervalLength);
-        this.setState({ runInterval: newRunInterval })
-      }
-    }
-  }
-
-  handlePlay() {
-    if (this.state.playbackInterval) {
-      clearInterval(this.state.playbackInterval);
-      this.setState({ playbackInterval: null });
-    }
-    
-    this.handleBeat();
-
-    this.setState({
-      isPlaying: true,
-      playbackInterval: setInterval(this.handleBeat, 60000 / this.props.tempo)
-    });
-  }
-
-  handleStop() {
-    clearInterval(this.state.playbackInterval);
-    this.setState({ isPlaying: false, currentPlayingLine: -1, playbackInterval: null });
-    this.props.onVirtualKeys([]);
-  }
-
   getMarkers() {
+    console.log(this.props.markerConfig);
     return [{
-      startRow: this.state.currentPlayingLine,
-      endRow: this.state.currentPlayingLine + 1,
-      className: this.state.isPlaying ? 'line-highlight' : '',
+      startRow: this.props.markerConfig.line,
+      endRow: this.props.markerConfig.line,
+      startCol: this.props.markerConfig.char,
+      endCol: this.props.markerConfig.char + 1,
+      className: this.props.markerConfig.show ? 'line-highlight' : '',
       type: 'background'
     }];
   }
 
   render() {
     return (
-      <>
-        <button onClick={ this.handlePlay }>Play</button>
-        <button onClick={ this.handleStop }>Stop</button>
-        <AceEditor
-          value={ this.state.code }
-          mode="java"
-          theme="github"
-          onChange={ c => this.setState({ code: c }) }
-          name="UNIQUE_ID_OF_DIV"
-          editorProps={{ $blockScrolling: true }}
-          markers={ this.getMarkers() }
-        />
-      </>
+      <AceEditor
+        value={ this.props.code }
+        mode="java"
+        theme="github"
+        onChange={ this.props.onCodeChange }
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        markers={ this.getMarkers() }
+      />
     );
   }
 }
