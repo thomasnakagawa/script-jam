586bcd5172b321d2914082b2d0997ad8fad2a6d4
Thomas Nakagawa <thomasnakagawa@gmail.com>
2020-04-04 17:07:07 -0700

    Updates

---
+++ precache-manifest.572b59e247b4c763b70d7141524c0acc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8548a375f7f67e5ef91c03a00e3ca4c8",
+    "url": "/script-jam/index.html"
+  },
+  {
+    "revision": "fa190cdaeb2ae5f208a6",
+    "url": "/script-jam/static/css/main.3483cf93.chunk.css"
+  },
+  {
+    "revision": "327729cacb3399de8262",
+    "url": "/script-jam/static/js/2.3dc20e20.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/script-jam/static/js/2.3dc20e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa190cdaeb2ae5f208a6",
+    "url": "/script-jam/static/js/main.0cb84cc8.chunk.js"
+  },
+  {
+    "revision": "f65530959fc3347f9c41",
+    "url": "/script-jam/static/js/runtime-main.5dc943bf.js"
+  },
+  {
+    "revision": "7cb09c758144c20a45f74e3411cbca63",
+    "url": "/script-jam/static/media/HIHAT1.7cb09c75.wav"
+  },
+  {
+    "revision": "226fbf2c064fc543b294a5b6a380ec5c",
+    "url": "/script-jam/static/media/KICK1.226fbf2c.wav"
+  },
+  {
+    "revision": "3efa79aaa015e4d6c7b2cec886ef44de",
+    "url": "/script-jam/static/media/SNARE4.3efa79aa.wav"
+  },
+  {
+    "revision": "bc3432be1a6d2c322c9f75780d73b0ae",
+    "url": "/script-jam/static/media/bass.bc3432be.wav"
+  },
+  {
+    "revision": "900acd2da3ad12b969eced22626bbe30",
+    "url": "/script-jam/static/media/jazz_sample_django.900acd2d.wav"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/script-jam/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3fb56212249121c686685a2d4a820cd9",
+    "url": "/script-jam/static/media/synth2.3fb56212.ogg"
+  },
+  {
+    "revision": "3c1ccfc4e71fd5f2568a18f22c955762",
+    "url": "/script-jam/static/media/synth_note.3c1ccfc4.ogg"
+  }
+]);
